27a56d31f11d6b64c0c62806c87cf0c7ba77c265
Pavel Kubica <pavel.kubica1@seznam.cz>
2024-09-06 11:16:05 +0200

    Add constants to aid with castling squares

--- src/app/game/_gamelogic/castles.ts
+++ src/app/game/_gamelogic/castles.ts
@@ -4,3 +4,11 @@ export enum Castles
     SHORT,
     LONG
 }
+
+export const CSTL_KING_SRC_FILE = "e";;
+export const SHORT_CSTL_KING_TARGET_FILE = "g";
+export const SHORT_CSTL_ROOK_SRC_FILE = "h";
+export const SHORT_CSTL_ROOK_TARGET_FILE = "f";
+export const LONG_CSTL_KING_TARGET_FILE = "c";
+export const LONG_CSTL_ROOK_SRC_FILE = "a";
+export const LONG_CSTL_ROOK_TARGET_FILE = "d";
