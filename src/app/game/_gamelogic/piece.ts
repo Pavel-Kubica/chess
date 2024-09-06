0415b4dd7706a1417783f9019838d2006e3d2d0b
Pavel Kubica <pavel.kubica1@seznam.cz>
2024-09-06 09:06:06 +0200

    Make possibleMoves implementations more readable for King and Pawn

--- src/app/game/_gamelogic/piece.ts
+++ src/app/game/_gamelogic/piece.ts
@@ -49,17 +49,28 @@ export class King extends Piece
 {
     possibleMoves(from: Square, color: Color, board: Board): BoardMove[]
     {
+        let retArr: BoardMove[] = [];
         let reachable = King.reachableExistentSquares(from);
+        retArr = retArr.concat(King.getPossibleSimpleMoves(from, color, board, reachable))
+        retArr = retArr.concat(King.getPossibleCaptures(from, color, board, reachable))
+        retArr = retArr.concat(King.getPossibleCastles(from, color, board))
+        return retArr;
+    }
 
-        // Moves
-        let retArr: BoardMove[] = reachable.filter(square => !board.at(square)) // No piece at target square
-                                    .map(square=> new BoardMove(from, square) )
+    private static getPossibleSimpleMoves(from: Square, color: Color, board: Board, reachable: Square[]): BoardMove[]
+    {
+        return reachable.filter(square => !board.at(square)) // No piece at target square
+                        .map(square=> new BoardMove(from, square))
 
-        // Captures
-        retArr = retArr.concat(reachable.filter(square => board.at(square)?.color !== color) // Piece of other color on target square -> can capture
-                                    .map(square=> new BoardMove(from, square, true) ))
-
-        // Castles
+    }
+    private static getPossibleCaptures(from: Square, color: Color, board: Board, reachable: Square[]): BoardMove[]
+    {
+        return reachable.filter(square => board.at(square)?.color !== color) // Piece of other color on target square -> can capture
+                        .map(square=> new BoardMove(from, square, true))
+    }
+    private static getPossibleCastles(from: Square, color: Color, board: Board): BoardMove[]
+    {
+        let retArr: BoardMove[] = []
         const castleRank = baseRank(color);
         if (JSON.stringify(from) === JSON.stringify(new Square("e", castleRank))) // King is at his starting spot
         {
@@ -75,7 +86,6 @@ export class King extends Piece
                 retArr.push(new BoardMove(from, from.left()!.left()!, false, Castles.LONG))
             }
         }
-
         return retArr;
     }
 
@@ -92,9 +102,16 @@ export class Pawn extends Piece
     possibleMoves(from: Square, color: Color, board: Board): BoardMove[]
     {
         let retArr: BoardMove[] = [];
-        let [aheadSquare, doubleMoveSquare, leftCaptureSquare, rightCaptureSquare] = Pawn.reachableSquares(from, color);
+        retArr = retArr.concat(Pawn.getPossibleSimpleMoves(from, color, board));
+        retArr = retArr.concat(Pawn.getPossibleCaptures(from, color, board));
+        return retArr;
+    }
 
-        // Moves
+    private static getPossibleSimpleMoves(from: Square, color: Color, board: Board): BoardMove[]
+    {
+        let retArr: BoardMove[] = [];
+        let [aheadSquare, doubleMoveSquare] = Pawn.reachableMoveSquares(from, color);
+
         if (!board.at(aheadSquare!)) // Pawn can't be on last rank
         { // If the square ahead is not occupied
             retArr.push(new BoardMove(from, aheadSquare!));
@@ -105,19 +122,25 @@ export class Pawn extends Piece
             }
         }
 
-        // Captures
+        return retArr;
+    }
+    private static getPossibleCaptures(from: Square, color: Color, board: Board): BoardMove[]
+    {
+        let retArr: BoardMove[] = [];
+        let [leftCaptureSquare, rightCaptureSquare] = Pawn.reachableCaptureSquares(from, color);
+
         if (leftCaptureSquare)
         {
             leftCaptureSquare = leftCaptureSquare!
             if (
                 // En passant could be possible to the left
                 (from.rank === enPassantRank(color) &&
-                !board.at(leftCaptureSquare) &&
-                board.at(from.left()!))
+                    !board.at(leftCaptureSquare) &&
+                    board.at(from.left()!))
                 ||
                 // Or there is simply an opponent's piece on the left capture square
                 (board.at(leftCaptureSquare)?.color !== color)
-                )
+            )
             {
                 retArr.push(new BoardMove(from, leftCaptureSquare, true))
             }
@@ -128,32 +151,36 @@ export class Pawn extends Piece
             if (
                 // En passant could be possible to the right
                 (from.rank === enPassantRank(color) &&
-                !board.at(rightCaptureSquare) &&
-                board.at(from.right()!))
+                    !board.at(rightCaptureSquare) &&
+                    board.at(from.right()!))
                 ||
                 // Or there is simply an opponent's piece on the right capture square
                 (board.at(rightCaptureSquare)?.color !== color)
-                )
+            )
             {
                 retArr.push(new BoardMove(from, rightCaptureSquare, true))
             }
         }
         return retArr;
     }
-
     /**
      * @returns [square ahead, doublemove target square, left capture square, right capture square]
      */
-    private static reachableSquares(from: Square, color: Color): [Square | undefined, Square | undefined, Square | undefined, Square | undefined]
+    private static reachableMoveSquares(from: Square, color: Color): [Square | undefined, Square | undefined]
     {
         if (color === Color.WHITE)
         {
-            return [from.above(), from.above()?.above(), from.topLeft(), from.topRight()]
+            return [from.above(), from.above()?.above()]
         }
-        else
+        return [from.below(), from.below()?.below()]
+    }
+    private static reachableCaptureSquares(from: Square, color: Color): [Square | undefined, Square | undefined]
+    {
+        if (color === Color.WHITE)
         {
-            return [from.below(), from.below()?.below(), from.bottomLeft(), from.bottomRight()]
+            return [from.topLeft(), from.topRight()]
         }
+        return [from.bottomLeft(), from.bottomRight()]
     }
 }
 
@@ -210,7 +237,7 @@ abstract class StraightLineMovingPiece extends Piece
             }
         }
 
-        return retArr
+        return retArr;
     }
 
 }
